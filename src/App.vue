30e7a0693102a8bdf1e671e728fe9c29230b6736
Jose Ángel Peña Díaz <penadiazjoseangel@gmail.com>
2026-07-16 15:08:54 +0200

    push to work from home

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<script setup>
+    import Header from './components/Header.vue'
+</script>
+<template>
+    <Header />
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+<template>
+    <nav class="navbar navbar-expand sticy-top bg-body-tertiary shadow-sm">
+        <div class="container">
+            <RouterLink class="navbar-brand" to="/">Calculator</RouterLink>
+            <div class="d-flex gap-3">
+                <RouterLink class="nav-link" to="/">Currency</RouterLink>
+                <RouterLink class="nav-link" to="/">Weather</RouterLink>
+            </div>
+        </div>
+    </nav>
+</template>
+<style>
+</style>
