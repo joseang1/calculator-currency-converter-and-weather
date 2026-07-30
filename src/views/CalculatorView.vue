ce8b8fd48d8c176e01ace3e78a7b93a8f80046d1
Jose Ángel Peña Díaz <penadiazjoseangel@gmail.com>
2026-07-30 12:42:48 +0200

    style: Fixed styles for the calculator

--- src/views/CalculatorView.vue
+++ src/views/CalculatorView.vue
@@ -38,42 +38,65 @@
 
 <template>
     <div class="container d-flex justify-content-center mt-5">
-        <div class="card shadow-lg" style="max-width: 360px; width: 100%;">
+        <div class="card shadow-lg" style="max-width: 360px; widt%;">
             <div class="card-body bg-dark text-white rounded">
                 <div class="bg-black text-white text-end p-3 mb-3 rounded fs-2 text-truncate">
                     {{ display }}
                 </div>
 
-                <div class="row g-2">
-                    <div class="col-3"><button class="btn btn-danger w-100" @click="resetAll">CE</button></div>
-                    <div class="col-3"><button class="btn btn-secondary w-100" @click="pressOperator('/')">/</button></div>
-                    <div class="col-3"><button class="btn btn-secondary w-100" @click="pressOperator('*')">*</button></div>
-                    <div class="col-3"><button class="btn btn-secondary w-100" @click="pressOperator('-')">-</button></div>
+                <div class="calc-grid">
+                    <button class="btn btn-danger" @click="resetAll">CE</button>
+                    <button class="btn btn-secondary" @click="pressOperator('/')">/</button>
+                    <button class="btn btn-secondary" @click="pressOperator('*')">*</button>
+                    <button class="btn btn-secondary" @click="pressOperator('-')">-</button>
                     
-                    <div class="col-3"><button class="btn btn-outline-light w-100" @click="pressNumber(7)">7</button></div>
-                    <div class="col-3"><button class="btn btn-outline-light w-100" @click="pressNumber(8)">8</button></div>
-                    <div class="col-3"><button class="btn btn-outline-light w-100" @click="pressNumber(9)">9</button></div>
-                    <div class="col-3"><button class="btn btn-warning w-100 h-100" @click="pressOperator('+')">+</button></div>
+                    <button class="btn btn-outline-light" @click="pressNumber(7)">7</button>
+                    <button class="btn btn-outline-light" @click="pressNumber(8)">8</button>
+                    <button class="btn btn-outline-light" @click="pressNumber(9)">9</button>
+                    <button class="btn btn-warning plus-btn" @click="pressOperator('+')">+</button>
 
-                    <div class="col-3"><button class="btn btn-outline-light w-100" @click="pressNumber(4)">4</button></div>
-                    <div class="col-3"><button class="btn btn-outline-light w-100" @click="pressNumber(5)">5</button></div>
-                    <div class="col-3"><button class="btn btn-outline-light w-100" @click="pressNumber(6)">6</button></div>
+                    <button class="btn btn-outline-light" @click="pressNumber(4)">4</button>
+                    <button class="btn btn-outline-light" @click="pressNumber(5)">5</button>
+                    <button class="btn btn-outline-light" @click="pressNumber(6)">6</button>
 
-                    <div class="col-3"><button class="btn btn-outline-light w-100" @click="pressNumber(3)">3</button></div>
-                    <div class="col-3"><button class="btn btn-outline-light w-100" @click="pressNumber(2)">2</button></div>
-                    <div class="col-3"><button class="btn btn-outline-light w-100" @click="pressNumber(1)">1</button></div>
-                    <div class="col-3"><button class="btn btn-warning w-100 h-100" @click="pressEquals">=</button></div>
+                    <button class="btn btn-outline-light" @click="pressNumber(3)">3</button>
+                    <button class="btn btn-outline-light" @click="pressNumber(2)">2</button>
+                    <button class="btn btn-outline-light" @click="pressNumber(1)">1</button>
+                    <button class="btn btn-warning equals-btn" @click="pressEquals">=</button>
 
-                    <div class="col-6"><button class="btn btn-outline-light w-100" @click="pressNumber(0)">0</button></div>
-                    <div class="col-3"><button class="btn btn-outline-light w-100" @click="pressDecimal">.</button></div>
-                </div>
-
+                    <button class="btn btn-outline-light zero-btn" @click="pressNumber(0)">0</button>
+                    <button class="btn btn-outline-light" @click="pressDecimal">.</button>
+                </div>  
             </div>
-        </div>
 
+           
+        </div>
     </div>
 </template>
 
-<style>
+<style scoped>
+
+    .calc-grid {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        gap: 0.5rem;
+    }
+
+    .calc-grid button {
+        width: 100%;
+        height: 100%;
+    }
+
+    .plus-btn {
+        grid-row: span 2;
+    }
+
+    .equals-btn {
+        grid-row: span 2;
+    }
+
+    .zero-btn {
+        grid-column: span 2;
+    }
 
 </style>
