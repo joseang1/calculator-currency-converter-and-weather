a834dbaced3f0d319290eaf444bb1ae45e00c62b
Jose Ángel Peña Díaz <penadiazjoseangel@gmail.com>
2026-08-25 12:07:19 +0200

    fix: deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/calculator-currency-converter-and-weather/',
   plugins: [vue()],
   server: {
     port: 5173
